6953ce410312ea84f3bc4cb4950dade396f2b9b1
Rodrigo Rodrigues Bezerra <50323864+RodrigoRodriguesBezerra@users.noreply.github.com>
2023-01-18 18:22:28 -0300

    Add files via upload

---
+++ Quicksort/quicksort/Quicksort.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <cstdlib>
+
+void selectionSortOP(int** v, int tamanho) {
+    int* ordenado = (int*)malloc(sizeof(int) * tamanho);
+
+    for (int j = 0; j < tamanho; j++) {
+
+        int i_min = 0;
+        for (int i = 0; i < tamanho; i++) {
+            if ((*v)[i] < (*v)[i_min]) {
+                i_min = i;
+            }
+        }
+        ordenado[j] = (*v)[i_min];
+        (*v)[i_min] = INT_MAX;
+    }
+    *v = ordenado;
+}
+
+void selectionSortIP(int* v, int tamanho) {
+    int aux = 0;
+    for (int j = 0; j < tamanho - 1; j++) {
+        int i_min = j;
+        for (int i = j + 1; i < tamanho; i++) {
+            if (v[i] < v[i_min]) {
+                i_min = i;
+            }
+        }
+        aux = v[j];
+        v[j] = v[i_min];
+        v[i_min] = aux;
+        //troca(v, i, i_min);
+    }
+}
+
+void quickSort(int* v, int ini, int fim) {
+    if (fim > ini) {
+        int indexPivo = particiona(v, ini, fim);
+        quickSort(v, ini, indexPivo - 1);
+        quickSort(v, indexPivo + 1, fim);
+    }
+}
+
+
+int particiona(int* v, int ini, int fim) {
+    int aleatorio = (rand() % (fim - ini)) + ini;
+    int aux = v[fim];
+    v[fim] = v[aleatorio];
+    v[aux] = aux;
+
+    int pIndex = ini;
+    int pivo = v[fim];
+    for (int i = ini; i < fim; i++) {
+        if (v[i] <= pivo) {
+            int temp = v[pIndex];
+            v[pIndex] = v[i];
+            v[i] = temp;
+            pIndex++;
+        }
+    }
+    int temp = v[pIndex];
+    v[pIndex] = v[fim];
+    v[fim] = temp;
+    return pIndex;
+}
